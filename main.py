6d9a9dddc2144af1e70590f0ee66f194635f4d53
xnilesh1 <160102391+xnilesh1@users.noreply.github.com>
2024-06-24 14:14:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import io
 import streamlit as st
 
+hide_st_style ="""
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+header {visibility: hidden;}
+</style>
+"""
+st.markdown(hide_st_style, unsafe_allow_html = True)
+
 
 
 try:
